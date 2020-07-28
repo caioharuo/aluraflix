340029b10a96418c19eb179ed91acfe57d5ec789
haruo <caio.haruo@hotmail.com>
2020-07-28 17:02:07 -0300

    Commit aula dois

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import Page404 from './pages/Page404';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Page404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ src/pages/Page404/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Page404Base } from './styles';
+
+function Page404(){
+    return(
+        <Page404Base>
+            <div className="Page404">404 - Not found</div>
+        </Page404Base>
+    )
+}
+
+export default Page404;
