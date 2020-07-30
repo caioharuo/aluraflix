520eda873afa8cc43c1d50a596e8b734fffdeeee
haruo <caio.haruo@hotmail.com>
2020-07-30 20:56:15 -0300

    Ajustes Cadastro de Categorias

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -31,7 +31,10 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias/';
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias/'
+      : 'https://haruoflix.herokuapp.com/categorias';
+
     fetch(URL)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
